35d0b30223557c2486498e984bb8294f2418d94d
Donovan Said <donovan.s.said@gmail.com>
2024-06-07 11:29:48 +0100

    Fixed eslint config and added props to app stacks.

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,8 +1,34 @@
 #!/usr/bin/env node
+
+/*
+- https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.Stack.html
+*/
+
 import * as cdk from 'aws-cdk-lib';
 import { QueueStack } from '../lib/cdk-queue-stack';
 import { LambdaStack } from '../lib/cdk-lambda-stack';
 
+// Environment configuration
+const dev = { account: '11111111', region: 'eu-west-1' };
+// const test = { account: '222222222', region: 'eu-west-2' };
+// const prod = { account: '333333333', region: 'eu-west-2' };
+
+// Global tags
+const tags = { "managed_by": "aws-cdk" };
+
+// App instantiation
 const app = new cdk.App();
-new QueueStack(app, 'QueueStack');
-new LambdaStack(app, 'LambdaStack');
+
+// Stacks
+new QueueStack(app, 'QueueStack', {
+    description: "SQS and SNS Stack",
+    stackName: "sample-app-queue-stack",
+    env: dev,
+});
+
+new LambdaStack(app, 'LambdaStack', {
+    description: "Lambda Stack",
+    stackName: "sample-app-lambda-stack",
+    tags: tags,
+    env: dev
+});
--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-
-
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
-  ...tseslint.configs.recommended,
-];
