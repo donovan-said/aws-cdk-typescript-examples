4fc5b555e3f5a7c56172529ac739cae2901438b2
Donovan Said <donovan.s.said@gmail.com>
2024-06-07 16:58:40 +0100

    Added cdk context and updated CHANGELOG.

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -8,27 +8,28 @@ import * as cdk from 'aws-cdk-lib';
 import { QueueStack } from '../lib/cdk-queue-stack';
 import { LambdaStack } from '../lib/cdk-lambda-stack';
 
-// Environment configuration
-const dev = { account: '11111111', region: 'eu-west-1' };
-// const test = { account: '222222222', region: 'eu-west-2' };
-// const prod = { account: '333333333', region: 'eu-west-2' };
-
-// Global tags
-const tags = { "managed_by": "aws-cdk" };
-
 // App instantiation
 const app = new cdk.App();
+
+// Environment configuration
+const environ = "dev"
+const account = app.node.tryGetContext('account')[environ];
+const cdk_env = { account: account['account_id'], region: account['region'] };
+
+// Global tags
+const tags = app.node.tryGetContext('tags');
 
 // Stacks
 new QueueStack(app, 'QueueStack', {
     description: "SQS and SNS Stack",
     stackName: "sample-app-queue-stack",
-    env: dev,
+    tags: tags,
+    env: cdk_env,
 });
 
 new LambdaStack(app, 'LambdaStack', {
     description: "Lambda Stack",
     stackName: "sample-app-lambda-stack",
     tags: tags,
-    env: dev
+    env: cdk_env
 });
